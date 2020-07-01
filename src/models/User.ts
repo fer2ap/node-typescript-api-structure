987a7ed046688c1d9020eb55f2f32e6f0e57606f
fer2ap <pinto.faa@gmail.com>
2020-06-30 21:02:44 -0300

    mongoose config and express config

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,11 @@
-export class User {
-  name: string;
-  email: string;
-}
+import { mongo } from '../database/index';
+import { createSchema, Type } from 'ts-mongoose';
+
+const UserSchema = createSchema({
+  name: Type.string({ required: true }),
+  email: Type.string({ required: true, unique: true, index: true, lowercase: true }),
+  password: Type.string({ required: true, select: false }),
+  createdAt: Type.date({ default: Date.now })
+});
+
+export const User = mongo;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import express from 'express'
-import '@controllers/UsersController'
+import '@controllers/UsersController';
+import bodyParser from 'body-parser';
+import express from 'express';
+import { connect } from './database';
 
-const app = express()
-
+const app: express.Application = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+connect()
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+  return response.json({ message: 'Hello World' });
+});
 
-app.listen(3333)
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
